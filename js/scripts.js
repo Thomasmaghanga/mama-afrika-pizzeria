71cb3d4f49ec2d136b7980364b2c41c52a76db20
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-07-29 11:33:46 +0300

    Make stylesheet changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-        $("nav").addClass("shrink");    
-}
-else {
-    $("nav").removeClass("shrink");
-}
-});    
+$(document).ready(function() {
+	$(window).scroll(function() {
+  	if($(document).scrollTop() > 10) {
+    	$('#nav').addClass('change');
+    }
+    else {
+    $('#nav').removeClass('change');
+    }
+  });
+});
